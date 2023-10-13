808098174c4594f63a4642d3ee121741329059ca
B Delacruz <brianmdelacruz@gmail.com>
2023-10-13 12:22:25 -0400

    update services

--- js/toggle.js
+++ js/toggle.js
@@ -1,3 +1,13 @@
-function toggleContent(element) {
-    element.parentElement.classList.toggle("active");
+// JavaScript to toggle service content
+function toggleContent(button) {
+    var serviceCard = button.parentElement;
+    serviceCard.classList.toggle("active");
 }
+
+// Attach click event listeners to the toggle buttons
+var toggleButtons = document.querySelectorAll('.service-toggle');
+toggleButtons.forEach(function(button) {
+    button.addEventListener('click', function() {
+        toggleContent(button);
+    });
+});
